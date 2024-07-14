e80023deea48e114051b472526b08f746be619fa
chenyue <2511006757@qq.com>
2024-07-14 17:41:05 +0800

    update by chenyue

--- src/main/java/com/chenyue/experiment/thread/juc/LockSupportTest.java
+++ src/main/java/com/chenyue/experiment/thread/juc/LockSupportTest.java
@@ -9,20 +9,19 @@ import java.util.concurrent.locks.LockSupport;
  * LockSupport 线程阻塞类工具，它可以在任意位置让线程阻塞
  * 1. LockSupport 不需要获得任何锁
  * 2. LockSupport 不会抛出InterruptedException
+ * 3. LockSupport.park() 可以响应interrupt
+ * 4. LockSupport.park() 线程挂起后，不会释放持有的锁，这是和Object.wait() 或者 Condition.await() 方法之间的一个重要区别
+ *
  */
 public class LockSupportTest {
-    public final static Object u = new Object();
-    static ChangeObjectThread t1 = new ChangeObjectThread("t1");
-    static ChangeObjectThread t2 = new ChangeObjectThread("t2");
-    static class ChangeObjectThread extends Thread {
-        public ChangeObjectThread(String threadName) {
-            super.setName(threadName);
-        }
+    public final static Object lock = new Object();
+
+    static class ChangeObjectThread implements Runnable {
 
         @Override //重写
         public void run() {
-            synchronized (u) { // 为什么需要获得u的锁
-                System.out.println("in " + getName());
+            synchronized (lock) { // 为什么需要获得u的锁
+                System.out.println("in " + Thread.currentThread().getName());
                 LockSupport.park();
                 if (Thread.currentThread().isInterrupted()) { //发生了中断就为true
                     System.out.println(Thread.currentThread().getName() + "发生了中断");
@@ -33,6 +32,10 @@ public class LockSupportTest {
     }
 
     public static void main(String[] args) throws InterruptedException {
+//        ChangeObjectThread t1 = new ChangeObjectThread("t1");
+//        ChangeObjectThread t2 = new ChangeObjectThread("t2");
+        Thread t1 = new Thread(new ChangeObjectThread(), "t1");
+        Thread t2 = new Thread(new ChangeObjectThread(), "t2");
         t1.start();
         Thread.sleep(100);
         t2.start();
